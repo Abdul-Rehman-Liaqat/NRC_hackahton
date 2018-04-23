c7fe51467c02fcd1b6cb02c26e7ce41c7f741b32
Abdul Rehman Liaqat <arl@plista.com>
2018-04-23 10:06:54 +0200

    add code to branch

--- app.py
+++ app.py
@@ -10,17 +10,29 @@ Created on Sat Apr 21 15:57:57 2018
 # Download the twilio-python library from twilio.com/docs/libraries/python
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
+from twilio.rest import Client
 
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def sms_ahoy_reply():
     """Respond to incoming messages with a friendly SMS."""
-    # Start our response
-    resp = MessagingResponse()
+    account_sid = "ACa55a95ee55c5b213f6b6430df33fa845"
+    auth_token = "576888fcc67b7059b5d0b2ad1a5d0199"
+    client = Client(account_sid, auth_token)
+    print('message received')
+    sms=client.messages.list(to="+33644603688")
+    text_body=client.messages(str(sms[1]).split('sid=')[2].split('>')[0]).fetch().body
+    print(text_body)
+        # Start our response
+    resp = MessagingResponse()  
+
+    if(text_body=='Hi' or text_body=='hi'or text_body=='help'or text_body=='Help'or text_body=='Hi'or text_body=='hi 'or text_body=='I need help' or text_body=='I need help '):
+            # Add a message
+            resp.message("Please visit ICLA-NRC.com or download ICLA-NRC app or send us your problem 1-Legal 2-Water 3-Shelter 4-Education 5-Food Security 6-Camp Management")
+    else:
+            resp.message("Please share information on: 1- Where are you from? 2- Where are you now 3- How old are you? 4- What kind of help do you need?(Birth docs, Marriage docs, Passport, Land issue)")
     
-    # Add a message
-    resp.message("demo 2 for humanitarian hackathon ICLA")
 
     return str(resp)
 
@@ -48,7 +60,7 @@ from twilio.rest import Client
 account_sid = "ACa55a95ee55c5b213f6b6430df33fa845"
 auth_token = "576888fcc67b7059b5d0b2ad1a5d0199"
 client = Client(account_sid, auth_token)
-
-for sms in client.messages.list():
-    print(sms.to)
+client.messages.list
+sms=client.messages.list(to="+4917658674524",date_sent='2018-04-21')
+text_body=client.messages(str(sms[-1]).split('sid=')[2].split('>')[0]).fetch().body
 '''
--- mongoDB-app.py
+++ mongoDB-app.py
@@ -11,6 +11,8 @@ connect('icla_db',host='localhost',port=27017)
 
 from flask import Flask,render_template, url_for, request,redirect
 from flask_mongoengine import MongoEngine
+from twilio.twiml.messaging_response import MessagingResponse
+from twilio.rest import Client
 
 app = Flask(__name__)
 @app.route("/")
@@ -24,6 +26,20 @@ def legal():
 @app.route('/send_sms/')
 def send_sms():
     return render_template('send_sms.html')
+
+@app.route('/submit/', methods=['POST'])
+def submit():
+#    numbers=["+4917658674524","+4915751034233"]
+    numbers=["+4917658674524"]
+    account_sid = "ACa55a95ee55c5b213f6b6430df33fa845"
+    auth_token = "576888fcc67b7059b5d0b2ad1a5d0199"
+    for to_number in numbers:
+        client = Client(account_sid, auth_token)
+        client.messages.create(
+                to=to_number,
+                from_="+33644603688 ",
+                body=request.form['text'])
+    return 'You entered: {}'.format(request.form['text'])
 
 if __name__ == '__main__':
       app.run(host='0.0.0.0', port=1234,debug=True)
